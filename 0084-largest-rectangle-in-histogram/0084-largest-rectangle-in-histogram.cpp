169da10432114815a3d70c7bde8be48c02b8bfe9
Phani Bhusan Mohanty <129371230+Phani2425@users.noreply.github.com>
2024-04-10 20:48:03 +0530

    Time: 124 ms (41.81%), Space: 87.5 MB (27.75%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int size=heights.size();
+
+        vector<int>NSR(size,size); //NSR=NEXT SMALLER RIGHT
+        vector<int>NSL(size,-1); //NSL=NEXT SMALLER LEFT
+        //for finding nand filling NSR and NSL for each  element we need a stack
+        stack<int> st;
+
+        //FILLING NSR FOR EACH  ELEMENT
+        for(int i=0;i<size;i++){
+            while(!st.empty() && heights[i] < heights[st.top()]){
+                NSR[st.top()] = i; 
+                //unlike normal next greater and next smaller element we will store index here (in those questions we were storing element in vector)
+                //we will do this because storing index will help us to find out width of rectangle in future
+                st.pop();
+            }
+
+            st.push(i);
+        }
+
+        //before filling NSL we will clear the stack if incase there is something left in stack(if NSR of some index couldn't be founded)
+        //we are clearing the stack in intention that those remaining index might affect the result while filling NSL
+        while(!st.empty()){
+            st.pop();
+        }
+        
+        //FILLING NSl FOR EACH  ELEMENT
+        for(int i=size-1;i>=0;i--){
+            while(!st.empty() && heights[i] < heights[st.top()]){
+                NSL[st.top()] = i; 
+                //unlike normal next greater and next smaller element we will store index here (in those questions we were storing element in vector)
+                //we will do this because storing index will help us to find out width of rectangle in future
+                st.pop();
+            }
+
+            st.push(i);
+        }
+
+        //NOW WE WILL CALUCLATE THE MAXIMUM AREA RECTANGLE POSSIBLE IN THE HISTOGRAM
+        //HERE WE WILL TRAVERSE THE "heights" ARRAY AND FOR EACH INDEX i THE NSR INDEX OF THAT ELEMENT IS PRESENT IN ith INDEX OF NSR ARRAY AND THE NSL INDEX OF THAT ELEMENT IS PRESENT IN ith INDEX OF NSL ARRAY
+        //from these NSR and NSL indexes of an element we will get the width of rectangle if consider that element as minimum height of rectangle
+        //width of rectangle having minimum height of height[i] will be:- NSR[i]-NSL[i]-1
+        //and area will be :-width*height = { (NSR[i]-NSL[i]-1) * height[i] }
+
+        int maxArea=INT_MIN;
+        for(int i=0;i<size;i++){
+            int area=(NSR[i]-NSL[i]-1) * heights[i];
+            maxArea=max(maxArea,area);
+        }
+
+        return maxArea;
+    }
+};
