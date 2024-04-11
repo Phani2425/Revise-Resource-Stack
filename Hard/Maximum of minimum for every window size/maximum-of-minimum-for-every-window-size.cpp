2605e07f42c8286ba0c26cb578935b875c8a4c82
Phani Bhusan Mohanty <129371230+Phani2425@users.noreply.github.com>
2024-04-12 02:32:26 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Maximum of minimum for every window size/maximum-of-minimum-for-every-window-size.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to find maximum of minimums of every window size.
+    vector <int> maxOfMin(int arr[], int n)
+    {
+        //CREATING ANS VECTOR
+        vector<int> ans (n,0);//it will be of size n and each index will represent a window size
+        //means 0th index represent 1 window size,1stindex->2 window size,2nd index-> 3 window size etc
+        //value in each index will represent the maximum of the minimum of respective window size
+        //it is initialised with 0 because in future we will do comparisions so we have to initialise it with something smaller than possiblke values
+        
+        stack<int> st;//stack is required for finding the NSR(next smaller right) and NSL(next smaller left) of elements in the array
+        //why it is required???
+        //kain na ethi mu array  ra gote gote element ku utheiki pacharibi  ki tu kete window size jae minimum element or to minimum element habara range kete
+        //au ei range amaku miliba from formula:- (NSR INDEX - NSL INDEX - 1) ru
+        
+        for(int i=0;i<n;i++){
+            while(!st.empty() && arr[i]<arr[st.top()]){
+                int index=st.top();
+                st.pop();
+                int range;
+                if(st.empty()){
+                    range=i; //( i is equal to {i-(-1)-1} )
+                }
+                else{
+                   range=i-st.top()-1;
+                }
+                ans[range-1]=max(ans[range-1],arr[index]);
+            }
+            
+            st.push(i);
+        }
+        
+        //now if we couldn't find NSR for any element then that will still be in the stack
+        while(!st.empty()){
+            int index=st.top();
+            st.pop();
+            int range;
+            if(st.empty()){
+                range=n;
+            }
+            else{
+                range=n-st.top()-1;
+            }
+            
+             ans[range-1]=max(ans[range-1],arr[index]);
+        }
+        
+        //last step
+        for(int i=n-2;i>=0;i--){
+            ans[i]=max(ans[i],ans[i+1]);
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        Solution ob;
+        vector <int> res = ob.maxOfMin(a, n);
+        for (int i : res) cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
