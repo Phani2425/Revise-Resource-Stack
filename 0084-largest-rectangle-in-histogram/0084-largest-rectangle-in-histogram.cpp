bed0bf35ac5e78da1a9a0d75dccedd239ffafbbb
Phani Bhusan Mohanty <129371230+Phani2425@users.noreply.github.com>
2024-04-11 20:37:43 +0530

    Time: 94 ms (93.27%), Space: 79.5 MB (77.1%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,63 +1,49 @@
 class Solution {
+
+/*THIS IS THE MOST OPTIMISED APPROACH OF THIS PROBLEM WHERE WE CAN  SOLVE THIS ONLY IN ONE PASS AND WE WILL NOT  BE NEEDING ANY EXTRA SPACE TO STORE NSR AND NSL
+AS IN THAT LOOP WHEN WE WILL GET NSR AND NSL OF A ELEMENT "X" INSTANTLY WE WILL CALLCULATE THE AREA OF RECTANGLE HAVING HEIGHT OF "X" AND ALSO UPDATE THE "MAX_AREA" VARIABLE
+SO THERE IS NO NEED TO STORE THEM.......
+
+THIS SOLUTION INCLUDES A GREAT TRICK FOR FINDING  NSR AND  NSL FOR A ELEMENT AT ONCE SO THIS IS VERY VERY IMPORTANT */
+
 public:
     int largestRectangleArea(vector<int>& heights) {
         int size=heights.size();
-
-        vector<int>NSR(size,size); //NSR=NEXT SMALLER RIGHT
-        vector<int>NSL(size,-1); //NSL=NEXT SMALLER LEFT
-        //for finding and filling NSR and NSL for each  element we need a stack
         stack<int> st;
+        int max_area=INT_MIN;
 
-        //FILLING NSR FOR EACH  ELEMENT
         for(int i=0;i<size;i++){
-            while(!st.empty() && heights[i] < heights[st.top()]){
-                NSR[st.top()] = i; 
-                //unlike normal next greater and next smaller element we will store index here (in those questions we were storing element in vector)
-                //we will do this because storing index will help us to find out width of rectangle in future
+            while(!st.empty() && heights[i]<heights[st.top()]){
+                int index=st.top();
                 st.pop();
+                if(st.empty()){
+                    max_area=max(max_area,heights[index]*(i));
+                }
+                else{
+                    max_area=max(max_area,(heights[index] * (i-st.top()-1)));
+                }
             }
 
             st.push(i);
         }
 
-        //before filling NSL we will clear the stack if incase there is something left in stack(if NSR of some index couldn't be founded)
-        //we are clearing the stack in intention that those remaining index might affect the result while filling NSL
+        //HERE WE WILL HANDLE A CASE WHERE EVEN AFTER TRAVERSING THE WHOLE ARRAY WE COULDN'T FIND NSR OF SOME ELEMENTS
+        //MEANS THOSE ELEMENT ARE STILL IN THE STACK 
+        //FOR THEM INDEX OF THEIR NSR WILL BE "SIZE" OF THE INPUT ARRAY AS CONSIDERING THIS WE CAN FIND THE WIDTH OF THE RECTANGLE
         while(!st.empty()){
+            int index=st.top();
             st.pop();
-        }
-        
-        //FILLING NSl FOR EACH  ELEMENT
-        for(int i=size-1;i>=0;i--){
-            while(!st.empty() && heights[i] < heights[st.top()]){
-                NSL[st.top()] = i; 
-                //unlike normal next greater and next smaller element we will store index here (in those questions we were storing element in vector)
-                //we will do this because storing index will help us to find out width of rectangle in future
-                st.pop();
+            if(st.empty()){
+                max_area=max(max_area, heights[index]*size);
             }
-
-            st.push(i);
+            else{
+                max_area=max(max_area, (heights[index]*(size-st.top()-1)));
+            }
         }
 
-        //NOW WE WILL CALUCLATE THE MAXIMUM AREA RECTANGLE POSSIBLE IN THE HISTOGRAM
-        //HERE WE WILL TRAVERSE THE "heights" ARRAY AND FOR EACH INDEX i THE NSR INDEX OF THAT ELEMENT IS PRESENT IN ith INDEX OF NSR ARRAY AND THE NSL INDEX OF THAT ELEMENT IS PRESENT IN ith INDEX OF NSL ARRAY
-        //from these NSR and NSL indexes of an element we will get the width of rectangle if consider that element as minimum height of rectangle
-        //width of rectangle having minimum height of height[i] will be:- NSR[i]-NSL[i]-1
-        //and area will be :-width*height = { (NSR[i]-NSL[i]-1) * height[i] }
-
-        int maxArea=INT_MIN;
-        for(int i=0;i<size;i++){
-            int area=(NSR[i]-NSL[i]-1) * heights[i];
-            maxArea=max(maxArea,area);
-        }
-
-        return maxArea;
+        return max_area;
     }
 };
 
-/*THIS APPROACH WILL TAKE ATIME COPMLEXITY OF O(N) BUT HERE  WE NNEED TO TRAVERSE THE ARRAY 3 TIMES 
-AND THE SPACE COMPLEXITY OF THE SOLUTION IS O(2N) AS HERE WE ARE TAKING 2 EXTRA VECTORS FOR STORING NSR AND NSL FOR EACH ELEMENT
+//
 
-BUT THE THING IS THIS IS NOT THE MOST OPTIMISIED APPROACH AND RECRUITER MAY ASK TO OPTIMISE THIS MORE 
-
-SO WHAT IF I TELL THAT WE CAN SOLVE THIS  ONLY IN ONE PASS OR IN ONE LOOP AND ALSO WE WILL NOT BE NEEDING ANY EXTRA SPACE MEANS CONSTANT SPACE COMPLEXITY
-LET'S UNDERSTAAND THAT IN NEXT SOLUTION*/
