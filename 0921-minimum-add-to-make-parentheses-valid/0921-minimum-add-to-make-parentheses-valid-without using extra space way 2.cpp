24caf8eacdd1af00f6d7c84b9a241de2f48cc0da
Phani Bhusan Mohanty <129371230+Phani2425@users.noreply.github.com>
2024-04-12 09:32:50 +0530

    Update without using extra space way 2.cpp

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid-without using extra space way 2.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid-without using extra space way 2.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-      int opencount=0;
-      int addition=0;
+      int opencount=0;//YE INDIRECTLY BATAYEGA KI MUJHE PARENTHESIS KO VALID BANANE KELIYE RIGHT SIDE ME KITNE CLOSING BRACKETS LAGANA PADEGA
+
+      int addition=0;//YE INDIRECTLY BATAYEGA KI MUJHE PARENTHESIS KO VALID BANANE KELIYE LEFT SIDE ME KITNE OPENING BRACKETS LAGANA PADEGA
+
       for(char ch:s){
         if(ch=='('){
             opencount++;
@@ -16,6 +18,8 @@ public:
             }
         }
       }
+
+//NO OF LEFT BRACKET I HAVE TO PUT + NO OF RIGHT BRACKET I HAVE TO PUT = NO OF STEPS I HAVE TO TAKE TO MAKE PARENTHESIS VALID. 
 
       addition += opencount;
       return addition;
