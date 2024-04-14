db793a104dac76eda5d4bcebf5729335aaa6ff93
Phani Bhusan Mohanty <129371230+Phani2425@users.noreply.github.com>
2024-04-14 23:15:33 +0530

    Time: 350 ms (11.87%), Space: 700.4 MB (8.73%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
+//RECURSIVE APPROACH
+//yaha pe basically ham s se abc ko bar bar hatakar t form karne ki kosis karrahe hai
     bool isValid(string s) {
-        //EDGE CASE
-        if(s.length()<3){
-            return false;
-        }
-
-        vector<char> v;
-
-        for(char ch : s){
-            if(ch=='c' && v.size()>1 && v[v.size()-1]=='b' && v[v.size()-2]=='a'){
-                v.pop_back();
-                v.pop_back();
-            }
-            else{
-                v.push_back(ch);
-            }
-        }
-
-        if(v.size()>0){
-            return false;
-        }
+     //BASE CASE
+     if(s.length()==0){//agar string s khali hogaya matalab wo t me convert ho gaya
         return true;
+     }
+
+     //RECURSIVE CASE
+     else{
+        int fnd=s.find("abc");
+        if(fnd != string::npos){  //agar abc milgaya to fnd uska index store karega then ham baki string ko abc ke left aur right me todenge aur recursion ko call karenge
+            string left=s.substr(0,fnd);
+            string right=s.substr(fnd+3,s.size());
+            return isValid(left+right);
+        }
+        
+        //agar kisi point pe string s me "abc" nehi mila to else ke andar wala if nehi chalega to phir recursive call nehi hoga aur end me controll ayega aur false return hoga
+        return false;
+     }
     }
 };
