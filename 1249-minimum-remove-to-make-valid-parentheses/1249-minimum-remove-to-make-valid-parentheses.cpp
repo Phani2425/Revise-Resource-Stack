dd5b57935bd3bf42416f4039c659b61430cfd62e
Phani Bhusan Mohanty <129371230+Phani2425@users.noreply.github.com>
2024-04-14 15:53:22 +0530

    Time: 23 ms (36.06%), Space: 13.6 MB (20.57%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,45 +1,72 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        //here i noticed i can solve this by using onne stack only where ii will store index of brackkets
+        /*THERE WILL BE TWO CASES WHERE A PRENTTHESIS WILL BE INVALID:- 
+        CASE 1:- WHEN AT A POINT NO OF CLOSING PARENTHESIS IS MORE THAN THE NO OF OPENING PARENTHESES
+        ->we will handle this with a loop which will traverse the string from left towards right and whenever at any point no of closing par becomes more than no of opening
+        we will erase that extra closing par from the string
 
-        //HERE ONE SINGLE STACK WILL STORE THE INDEX OF BRACKETS TO CHECK WHEATHER THEY ARE VALID OR NOT
-        //AT END OF THE LOOP THIS STACK WILL STORE THE INDEX VALUES OF THOSE BRACKETS WHO ARE MAKING THE PARENTHESIS INVALID
-        //AT LAST WE WILL ERASE THOSE BRACKETS WHOSE INDEX IS REAMINED IN THIS STACK AND NOT GOT CANCELLED WITH ANYONE AND BY ERASING THEM WE WILL MAKE THE GIVEN PARENTHESES VALID
+        CASE 2:- WHEN THE NO OF OPENING PARENTHESIS IS MORE THAN THE NO OF CLOSING PARENTHESE 
+        ->we will handle this with a loop which will traverse the string from right towards left and whenever at any point no of opening par becomes more than no of closing
+        we will erase that extra opening par from the string*/
         stack<int> index;
-
+         
+        // CASE 1
         int len=s.length();
-        for(int i=0;i<len;i++){
-            if(isalpha(s[i])){
-                continue;
+        int open=0;
+        string result="";
+        for(char ch:s){
+            if(isalpha(ch)){
+                result.push_back(ch);
             }
             else{
-                if(index.empty() || s[i]=='('){ //if anyone condition is true then this will run
-                    index.push(i);
+               if(ch=='('){
+                result.push_back(ch);
+                open++;
+               }
+               else{
+                if(open==0){
+                    continue;
                 }
-                else{ //means bracket is closing
-                    if(s[index.top()]=='('){
-                       index.pop();
-                    }
-                    else{
-                        index.push(i);
-                    }
+                else{
+                    open--;
+                    result.push_back(ch);
                 }
+               }
             }
         }
 
-       if(index.empty()){
-          //means parentheses is already valid
-          return s;
-       } 
-       else{
-          while(!index.empty()){
-               //we will erase these indexes 
-               s.erase(index.top(),1);
-               index.pop();
-          }
-       }
+        if(open==0){
+            return result;
+        }
+        
 
-       return s;
+        //CASE 2
+        int r_len=result.length();
+        int close=0;
+        string final_result="";
+        for(int i=r_len-1;i>=0;i--){
+            if(isalpha(result[i])){
+                final_result.push_back(result[i]);
+            }
+            else{
+               if(result[i]==')'){
+                final_result.push_back(result[i]);
+                close++;
+               }
+               else{
+                if(close==0){
+                    continue;
+                }
+                else{
+                    close--;
+                    final_result.push_back(result[i]);
+                }
+               }
+            }
+        }
+
+    reverse(final_result.begin(),final_result.end());
+       return final_result;
     }
 };
