93ed86c2b0a2ca2453fd839ec69e5c583d7e22b2
Phani Bhusan Mohanty <129371230+Phani2425@users.noreply.github.com>
2024-04-14 22:08:51 +0530

    Time: 8 ms (48.2%), Space: 18 MB (38.34%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -3,6 +3,9 @@ public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
         int i=0,j=0;
         stack<int> st;
+
+        //WITH THE HELP OF THIS LOOP WE WILL REMOVE THOSE ELEMNT WHICH WERE POPED BETWEEN PUSING WAS GOING ON
+        //AFTER REMOVING THEM IN THE NEXT LOOP WE WILL HANDLE THE CONTINUOUS POP STATEMENTS
         while(i != pushed.size()){
            if(st.empty() || popped[j] != st.top()){
             st.push(pushed[i]);
@@ -14,6 +17,7 @@ public:
            }
         }
 
+        //ONCE ALL THE NON CONTINUOUS POP STATEMENT ARE COMMPLETED WE WILL HANDLE CONTINUOUS POP STATEMENTS HERE
         while(j != popped.size()){
             if(popped[j]==st.top()){
                 st.pop();
