b90ff8d548e82699a55c2af1e3202dacd5952b2b
Phani Bhusan Mohanty <129371230+Phani2425@users.noreply.github.com>
2024-04-14 22:37:01 +0530

    Time: 13 ms (52.28%), Space: 9.8 MB (42.92%) - LeetHub

---
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        //EDGE CASE
+        if(s.length()<3){
+            return false;
+        }
+
+        vector<char> v;
+
+        for(char ch : s){
+            if(ch=='c' && v.size()>1 && v[v.size()-1]=='b' && v[v.size()-2]=='a'){
+                v.pop_back();
+                v.pop_back();
+            }
+            else{
+                v.push_back(ch);
+            }
+        }
+
+        if(v.size()>0){
+            return false;
+        }
+        return true;
+    }
+};
